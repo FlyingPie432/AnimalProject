fe48a6847250d4211dd1d207b587ce258c37cf94
jamal <polchayevc@gmail.com>
2024-03-28 22:56:55 +0400

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,26 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+
+SECRET_KEY = os.urandom(16)
+path = Path(__file__).parent.absolute()
+SQLALCHEMY_DATABASE_URI = f'sqlite:///{path}/db/flask.db'
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+
+SESSION_FILE_DIR = os.path.join(path, 'sessions')
+SESSION_TYPE = 'filesystem'
+# SESSION_COOKIE_SECURE = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SAMESITE = 'Lax'
+REMEMBER_COOKIE_DURATION = 3600
+SESSION_PERMANENT = True
+PERMANENT_SESSION_LIFETIME = timedelta(days=7)
+
+MAIL_SERVER = 'smtp.gmail.com'
+MAIL_PORT = 587
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
